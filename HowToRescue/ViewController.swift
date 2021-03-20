afaee7c341f709fd413b4d4965bfeba9be0637b6
TakahashiTsubasa <win9it.124gmail.com>
2021-03-20 23:18:40 +0900

    テスト

--- HowToRescue/ViewController.swift
+++ HowToRescue/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("コミットテスト")
+        
         rescueBarView.backgroundColor = UIColor(red: 160 / 255, green: 51 / 255, blue: 34 / 255, alpha: 1)
     }
     
